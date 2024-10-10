02ecd7f691c1d32b5bad842deca3d6fd53774654
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-10 20:25:02 +0200

    admin added

---
+++ TravelTripProje/Models/Classes/Adress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Classes
+{
+    public class Adress
+    {
+        [Key]
+        public int AdressId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AdressName { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public string MapLocation { get; set; }
+    }
+}
---
+++ TravelTripProje/Models/Classes/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Classes
+{
+    public class Contact
+
+    {
+        [Key]
+        public int ContactId { get; set; }
+        public string NameSurName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; } 
+    }
+}
