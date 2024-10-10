b8cc54e81004a052f77063c84c448503ba6cd80f
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-10 20:20:23 +0200

    table erstelt

---
+++ TravelTripProje/Models/Classes/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Classes
+{
+    public class About
+    {
+        [Key]
+        public int AboutId { get; set; }
+        public string FotoUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
