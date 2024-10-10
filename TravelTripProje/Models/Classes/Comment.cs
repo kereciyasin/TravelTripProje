ef143ea3894f0ce85b0784cca312aa9f60f6974c
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-10 20:26:36 +0200

    blog class added

---
+++ TravelTripProje/Models/Classes/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Classes
+{
+    public class Comment
+    {
+        [Key] 
+        public int CommentId { get; set; }
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string Description { get; set; }
+        public int BlogId { get; set; } 
+    }
+}
