d0e45db4c51467105a6751de60a1b24517a83021
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-13 20:43:33 +0200

    blogDetails

---
+++ TravelTripProje/Models/Classes/BlogComment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TravelTripProje.Models.Classes;  
+
+namespace TravelTripProje.Models.Classes
+{
+    public class BlogComment
+    {
+        /* Iki tabloyu bir view de cekmek icin. */
+        public IEnumerable<Blog> Value1 { get; set; }   
+        public IEnumerable<Comment> Value2 { get; set; }
+
+    }
+}
