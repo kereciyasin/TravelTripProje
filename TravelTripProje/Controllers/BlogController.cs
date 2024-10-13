de375fded0b13d7da8a571d4d45f37d995209295
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-13 13:05:41 +0200

    blog update

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
+
 
 namespace TravelTripProje.Controllers
 {
     public class BlogController : Controller
     {
-        // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+
         }
     }
 }
