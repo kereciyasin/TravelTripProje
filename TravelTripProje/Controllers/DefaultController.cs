70a7291f4b3b345b93ce37591aabcf6609fb9924
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-14 15:01:28 +0200

    img height added

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Classes;   
 
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
-        
+        Context Context = new Context();
         public ActionResult Index()
         {
-            return View();
+            var values = Context.Blogs.ToList();    
+            return View(values);
         }
 
         public ActionResult About()
