90c3e8e6e419d7803e07489f8159150cf6059a26
Yasin Kereci <kereciyasin52@gmail.com>
2024-10-21 16:22:20 +0200

    31

--- TravelTripProje/Controllers/AdminController.cs
+++ TravelTripProje/Controllers/AdminController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Classes;   
+
 
 namespace TravelTripProje.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        Context c = new Context();  
         public ActionResult Index()
         {
-            return View();
+            var values = c.Blogs.ToList();
+            return View(values);
         }
     }
 }
